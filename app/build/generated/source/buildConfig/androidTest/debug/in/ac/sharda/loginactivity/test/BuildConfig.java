1d4d439f12b26393e0d5522d8782562f73236543
tuiba <tuiba.ashraf107@gmail.com>
2019-08-31 22:23:40 +0530

    Ecommerce application

---
+++ app/build/generated/source/buildConfig/androidTest/debug/in/ac/sharda/loginactivity/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package in.ac.sharda.loginactivity.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "in.ac.sharda.loginactivity.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
